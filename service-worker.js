6a39691307de95ef39576392648b060f9aa3b37c
naleyjanhelge <naleyjanhelge@gmail.com>
2025-04-23 11:52:20 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "gamenight-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/manifest.json",
+  "/public/gamenight_ikon.png",
+  "/public/GameNight-logo-small.webp",
+  "/src/games/example.json"
+];
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("[ServiceWorker] Pre-caching offline page");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    })
+  );
+});
